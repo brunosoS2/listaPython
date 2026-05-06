d6b054dd58e69dfb1d0cbd48b2b8f72851099725
brunosoS2 <braw87bruno@gmail.com>
2026-05-06 14:29:42 +0000

    II

--- quizz.py
+++ quizz.py
@@ -1,1 +1,12 @@
+#-- Quizz sobre mitologia grega--
 
+print(" Quiz de mitologia!🔱⛈️🦉")
+
+print("Na mitologia grega, quem é conhecido como rei dos deuses e senhor do Olimpo? \n A) Zeus \n B) Posseidon \n C) Apollo")
+pergunta_1 = input("qual a resposta correta? ")
+resposta_correta = "A"
+
+if pergunta_1 == "A":
+    print("correto!!✅")
+else:
+    print("errado!!")

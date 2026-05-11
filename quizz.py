bceeabfc125441ac609509f4fdae8f7fbda6234a
brunosoS2 <braw87bruno@gmail.com>
2026-05-11 00:49:01 +0000

    boa noite

--- quizz.py
+++ quizz.py
@@ -1,8 +1,8 @@
-#-- Quizz sobre mitologia grega--
+#-- Quizz--
 
-print(" Quiz de mitologia!🔱⛈️🦉")
+print(" Quiz conhecimentos gerais!🔱⛈️🦉")
 
-print("Na mitologia grega, quem é conhecido como rei dos deuses e senhor do Olimpo? \n A) Zeus \n B) Posseidon \n C) Apollo")
+print("1. Na mitologia grega, quem é conhecido como rei dos deuses e senhor do Olimpo? \n A) Zeus \n B) Posseidon \n C) Apollo")
 pergunta_1 = input("qual a resposta correta? "). upper()
 resposta_correta1 = "A"
 
@@ -11,11 +11,23 @@ if pergunta_1 == resposta_correta1:
 else:
     print("errado!!")
 
-print("Na mitologia grega, quem é conhecido como rei dos deuses e senhor do Olimpo? \n A) Zeus \n B) Posseidon \n C) Apollo")
-pergunta_1 = input("qual a resposta correta? "). upper()
-resposta_correta2 = "A"
+print("2. Quantas copas o Brasil possui? \n A) 3 \n B) 5 \n C) 7 ")
+pergunta_2 = input("qual a resposta correta? "). upper()
+resposta_correta2 = "B"
 
 if pergunta_2 == resposta_correta2:
     print("correto!!✅")
 else:
     print("errado!!")
+
+print("3. Qual é o maior planeta do nosso Sistema Solar e qual é a sua principal característica visível? \n A) saturno e seus aneis \n B) Jupiúpiter e sua grande mancha vermelha \n C) marte e o olympos mont")
+pergunta_3 = input("qual a resposta correta?"). upper()
+resposta_correta3 = "B"
+
+if pergunta_3 == resposta_correta3:
+    print("correto!!✅")
+else:
+    print("errado!!")
+
+#--Bruno Sousa De Almeida-2 E--
+
